2689a1837ddfd5bcd2c2b2871a2f9f195a115cea
Crowdbotics <engineering@crowdbotics.com>
2024-03-18 15:44:17 +0000

    Initial commit

---
+++ backend/sparkling_snow_dev_134577/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for sparkling_snow_dev_134577 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "sparkling_snow_dev_134577.settings"
+)
+
+application = get_wsgi_application()
